4a1624a97a6ed786265e0b22d4eb2f5cc90ce84c
dingsheng <727040141@qq.com>
2025-01-15 13:46:27 +0800

    chore: update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MenusModule } from './menus/menus.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { MenusModule } from './menus/menus.module';
     UsersModule,
     AuthModule,
     MenusModule,
+    RolesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
